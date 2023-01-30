bcb09d5f2c592caef85b237f475d6aea2437bddd
sharjeelafzaal123 <muhammad.sharjeel@codefrenetics.com>
2023-01-30 12:05:04 +0500

    adding user query, encryption of pasword, JWT

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
 //@route        GET api/auth
 //@desc         Test route
 //@access       Public
-router.get("/", (req, res) => res.send("Auth Route"));
+router.get("/", auth, async (req, res) => {
+
+    try {
+        if (req.user && req.user.id) {
+            const user = await User.findById(req.user.id).select("-password")
+            res.json(user);
+        }
+        else {
+            res.status(401).json({ message: "No user id found" });
+        }
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('server error');
+    }
+});
 
 module.exports = router;
