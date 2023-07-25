99ab35c8fe76f2c240100df25e1449d7e7f40736
sharjeelafzaal123 <muhammad.sharjeel@codefrenetics.com>
2023-07-25 17:43:47 +0500

    module.exports

--- server.js
+++ server.js
@@ -33,3 +33,5 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
+module.exports = app;
